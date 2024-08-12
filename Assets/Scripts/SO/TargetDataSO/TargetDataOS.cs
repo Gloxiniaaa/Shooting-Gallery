78b0ff75bf2ee5226948cb4238c0fc84d106a545
Gloxiniaaa <hnm142758@gamil.com>
2024-08-12 12:33:48 +0700

    target: swing, slide,...

--- Assets/Scripts/SO/TargetDataSO/TargetDataOS.cs
+++ Assets/Scripts/SO/TargetDataSO/TargetDataOS.cs
@@ -3,7 +3,26 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TargetDataOS", menuName = "TargetDataOS", order = 0)]
 public class TargetDataOS : ScriptableObject
 {
+    [Tooltip("Target s gonna randomly choose 1 of these sprites")]
+    [SerializeField] private Sprite[] _frontSrites;
+    [SerializeField] private Sprite[] _stickSpites;
     public Sprite BackSprite;
     public float FlipDuration;
     public Vector3 EndFlipRotation;
+    public float Height;
+    public float SlideSpeed;
+    public float SwingRange;
+    public float SwingDuration;
+
+    public Sprite RandomFrontSprite()
+    {
+        int idx = Random.Range(0, _frontSrites.Length);
+        return _frontSrites[idx];
+    }
+
+    public Sprite RandomStickSprite()
+    {
+        int idx = Random.Range(0, _stickSpites.Length);
+        return _stickSpites[idx];
+    }
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using Unity.Mathematics;
 using UnityEngine;
 
 [RequireComponent(typeof(CircleCollider2D))]
@@ -6,6 +7,24 @@ public class Target : MonoBehaviour, IShootable
 {
     [SerializeField] private TargetDataOS _targetDataOS;
     [SerializeField] private SpriteRenderer _targetSpriteRenderer;
+    [SerializeField] private SpriteRenderer _StickSpriteRenderer;
+    private int _direction = 1;
+    private bool _isShot;
+
+    private void OnEnable()
+    {
+        transform.localRotation = quaternion.identity;
+        _isShot = false;
+        ChooseSprite();
+        AppearAndSwing();
+        Slide();
+    }
+
+    private void ChooseSprite()
+    {
+        _targetSpriteRenderer.sprite = _targetDataOS.RandomFrontSprite();
+        _StickSpriteRenderer.sprite = _targetDataOS.RandomStickSprite();
+    }
 
     private void OnMouseDown()
     {
@@ -14,14 +33,38 @@ public class Target : MonoBehaviour, IShootable
 
     public void OnShot()
     {
-        Flip();
+        if (!_isShot)
+        {
+            _isShot = true;
+            FlipAndDisappear();
+        }
     }
 
-    private void Flip()
+    private void FlipAndDisappear()
     {
         Sequence sequence = DOTween.Sequence();
-        sequence.Append(transform.DOLocalRotate(_targetDataOS.EndFlipRotation / 2, _targetDataOS.FlipDuration / 3).OnComplete(
+        sequence.Append(transform.DOLocalRotate(_targetDataOS.EndFlipRotation / 2f, _targetDataOS.FlipDuration * 0.4f).OnComplete(
             () => _targetSpriteRenderer.sprite = _targetDataOS.BackSprite));
-        sequence.Append(transform.DOLocalRotate(_targetDataOS.EndFlipRotation, _targetDataOS.FlipDuration * 2 / 3).SetEase(Ease.OutBounce));
+        sequence.Append(transform.DOLocalRotate(_targetDataOS.EndFlipRotation, _targetDataOS.FlipDuration * 0.6f).SetEase(Ease.OutBounce));
+        sequence.AppendInterval(0.5f);
+        sequence.Append(transform.DOLocalMoveY(transform.localPosition.y - _targetDataOS.Height, 0.3f).SetEase(Ease.InBack));
+        sequence.OnComplete(() => gameObject.SetActive(false));
+    }
+
+    private void AppearAndSwing()
+    {
+        transform.DOLocalMoveY(transform.localPosition.y + _targetDataOS.Height, 0.5f).SetEase(Ease.OutBack).OnComplete(
+            () => transform.DOLocalMoveY(transform.localPosition.y + _targetDataOS.SwingRange, _targetDataOS.SwingDuration).SetEase(Ease.InOutQuad).SetLoops(-1, LoopType.Yoyo)
+        );
+    }
+
+    private void Slide()
+    {
+        transform.DOLocalMoveX(transform.localPosition.x + _direction * 15, 15 / _targetDataOS.SlideSpeed).SetEase(Ease.Linear);
+    }
+
+    private void OnDisable()
+    {
+        transform.DOKill();
     }
 }
