96a840fa327245adcccb996d8092ed0903b16488
Gloxiniaaa <hnm142758@gamil.com>
2024-08-15 21:52:22 +0700

    count down timer

--- Assets/Scripts/Services/TimeCounter.cs
+++ Assets/Scripts/Services/TimeCounter.cs
@@ -1,8 +1,44 @@
+using TMPro;
 using UnityEngine;
 
 public class TimeCounter : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI TimerText;
+    [SerializeField] private int _initialTimer;
+    private int _countDown = 0;
+
+    [Header("Broadcast on channel:")]
+    [SerializeField] private VoidEventChannelSO TimesUpEvent;
+
+
     [Header("Listen on channel:")]
     [SerializeField] private VoidEventChannelSO StartGameEvent;
-    
+
+    private void OnEnable()
+    {
+        _countDown = _initialTimer;
+        TimerText.text = _countDown.ToString();
+        StartGameEvent.OnEventRaised += CountDown;
+    }
+
+    private void CountDown()
+    {
+        InvokeRepeating(nameof(Tick), 1f, 1f);
+    }
+
+    private void Tick()
+    {
+        _countDown--;
+        TimerText.text = _countDown.ToString();
+        if (_countDown == 0)
+        {
+            TimesUpEvent.RaiseEvent();
+            CancelInvoke();
+        }
+    }
+
+    private void OnDisable()
+    {
+        StartGameEvent.OnEventRaised -= CountDown;
+    }
 }
